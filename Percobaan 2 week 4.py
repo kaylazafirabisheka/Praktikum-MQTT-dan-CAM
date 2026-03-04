9c4c2cc4ef1292c957f7781701520417bd115d22
kaylazafirabisheka <zafirabisheka10@gmail.com>
2026-03-04 22:49:09 +0700

    Add files via upload

---
+++ Percobaan 2 week 4.py
@@ -0,0 +1,12 @@
+import paho.mqtt.client as mqtt
+subscribe = "test"
+def on_message(client, userdata, msg):
+    data= str(msg.payload.decode())
+    print(data)
+    return
+client = mqtt.Client()
+client.on_message = on_message
+server = "mqtt-dashboard.com"
+client.connect(server, 1883)
+client.subscribe(subscribe)
+client.loop_forever()
